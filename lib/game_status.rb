fcc2d9c5627121caf92904de6e66611943d72024
Red Hedonist <vladimirtereshchuk@yahoo.com>
2019-08-17 11:36:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,39 +4,49 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+
 WIN_COMBINATIONS = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [6, 4, 2]
+  [0,1,2], # Top row
+  [3,4,5],  # Middle row
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
 ]
 
+
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each do |win_combination|
 
-    position_1 = board[index_0]
-    position_2 = board[index_1]
-    position_3 = board[index_2]
+  win_index_1 = win_combination[0]
+  win_index_2 = win_combination[1]
+  win_index_3 = win_combination[2]
 
-    if position_1 == "X" && position_2 == "X" && position_3 == "X"
-      return win_combo
-    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
-      return win_combo
+  position_1 = board[win_index_1] 
+  position_2 = board[win_index_2] 
+  position_3 = board[win_index_3] 
+  
+    if (position_1 == "X" && position_2 == "X" && position_3 == "X") || (position_1 == "O" && position_2 == "O" && position_3 == "O")
+      return win_combination
     end
-  }
-  return false
+  end
+  false
 end
+
 
 def full?(board)
-  board.all? {|index| index == "X" || index == "O"}
+  counter = 0
+  while counter < 9
+    if !position_taken?(board,counter)
+      return false
+    end
+    counter += 1
+  end
+  true
 end
+
 
 def draw?(board)
   if !won?(board) && full?(board)
@@ -46,24 +56,20 @@ def draw?(board)
   end
 end
 
+
 def over?(board)
-  if won?(board) || full?(board) || draw?(board)
+  if won?(board) || draw?(board) || full?(board)
     return true
   else
     return false
   end
 end
 
-def winner (board)
-  index = []
-  index = won?(board)
-  if index == false
-    return nil
+
+def winner(board)
+  if won?(board) && !draw?(board)
+    return board[won?(board)[0]]
   else
-    if board[index[0]] == "X"
-      return "X"
-    else
-      return "O"
-    end
+    return nil
   end
 end
